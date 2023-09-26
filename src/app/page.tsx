19e6b16fdddb3f213abbda0f8b0c9c8a383011ae
wspawned <wspawned@gmail.com>
2023-09-27 01:37:10 +0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="max-w-5xl w-full h-full items-center justify-center font-mono text-sm lg:flex">
+        <p>MOVIES APP</p>
+      </div>
+    </main>
+  )
+}
